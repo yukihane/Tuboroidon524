58a93e617c0a95746fc02a61e3db9ccf8596076a
yukihane <yukihane.feather@gmail.com>
2012-06-12 21:08:32 +0900

    コンパイルエラー解消用のダミークラス追加

---
+++ NLib/src/jp/syoboi/android/ListViewScroller.java
@@ -0,0 +1,12 @@
+package jp.syoboi.android;
+
+import android.widget.ListView;
+
+public class ListViewScroller {
+
+    public void scroll(ListView listView, float f, int scrollAnimationTime) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
