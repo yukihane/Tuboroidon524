5e668a2050f63cdef343d3d66cc391fe7e81c5b8
yukihane <yukihane.feather@gmail.com>
2014-12-31 14:28:06 +0900

    定数を別クラスに分離

--- NLib/src/info/narazaki/android/lib/activity/Config.java
+++ NLib/src/info/narazaki/android/lib/activity/Config.java
@@ -1,5 +1,28 @@
 package info.narazaki.android.lib.activity;
 
+import static info.narazaki.android.lib.activity.Constants.INTENT_KEY_ALERT_OVERWRITE;
+import static info.narazaki.android.lib.activity.Constants.INTENT_KEY_ALLOW_NEW_DIR;
+import static info.narazaki.android.lib.activity.Constants.INTENT_KEY_ALLOW_NEW_FILE;
+import static info.narazaki.android.lib.activity.Constants.INTENT_KEY_CHECK_WRITABLE;
+import static info.narazaki.android.lib.activity.Constants.INTENT_KEY_CURRENT;
+import static info.narazaki.android.lib.activity.Constants.INTENT_KEY_DEFAULT_NEW_FILENAME;
+import static info.narazaki.android.lib.activity.Constants.INTENT_KEY_FILE_EXTENTION;
+import static info.narazaki.android.lib.activity.Constants.INTENT_KEY_FILE_PATTERN;
+import static info.narazaki.android.lib.activity.Constants.INTENT_KEY_FONT_SIZE;
+import static info.narazaki.android.lib.activity.Constants.INTENT_KEY_LIGHT_THEME;
+import static info.narazaki.android.lib.activity.Constants.INTENT_KEY_NEW_DIR_CAPTION;
+import static info.narazaki.android.lib.activity.Constants.INTENT_KEY_NEW_DIR_TITLE;
+import static info.narazaki.android.lib.activity.Constants.INTENT_KEY_NEW_FILE_CAPTION;
+import static info.narazaki.android.lib.activity.Constants.INTENT_KEY_NEW_FILE_HINT;
+import static info.narazaki.android.lib.activity.Constants.INTENT_KEY_NEW_FILE_TITLE;
+import static info.narazaki.android.lib.activity.Constants.INTENT_KEY_PICK_DIRECTORY;
+import static info.narazaki.android.lib.activity.Constants.INTENT_KEY_PICK_DIR_CAPTION;
+import static info.narazaki.android.lib.activity.Constants.INTENT_KEY_RECENT_DIR_KEEP_TAG;
+import static info.narazaki.android.lib.activity.Constants.INTENT_KEY_ROOT;
+import static info.narazaki.android.lib.activity.Constants.INTENT_KEY_SLECTION_ALERT_MESSAGE;
+import static info.narazaki.android.lib.activity.Constants.INTENT_KEY_SLECTION_ALERT_TITLE;
+import static info.narazaki.android.lib.activity.Constants.INTENT_KEY_TITLE;
+import static info.narazaki.android.lib.activity.Constants.INTENT_KEY_WRITE_FAILED_MESSAGE;
 import info.narazaki.android.lib.R;
 
 import java.io.File;
@@ -11,38 +34,6 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 
 final class Config {
-
-    public static final String INTENT_KEY_LIGHT_THEME = "info.narazaki.android.lib.extra.LIGHT_THEME";
-
-    public static final String INTENT_KEY_TITLE = "info.narazaki.android.lib.extra.TITLE";
-    public static final String INTENT_KEY_ROOT = "info.narazaki.android.lib.extra.ROOT";
-    public static final String INTENT_KEY_CURRENT = "info.narazaki.android.lib.extra.CURRENT";
-    public static final String INTENT_KEY_FONT_SIZE = "info.narazaki.android.lib.extra.FONT_SIZE";
-    public static final String INTENT_KEY_DEFAULT_NEW_FILENAME = "info.narazaki.android.lib.extra.NEW_FILENAME";
-    public static final String INTENT_KEY_NEW_FILE_HINT = "info.narazaki.android.lib.extra.NEW_FILE_HINT";
-    public static final String INTENT_KEY_ALLOW_NEW_DIR = "info.narazaki.android.lib.extra.ALLOW_NEW_DIR";
-    public static final String INTENT_KEY_ALLOW_NEW_FILE = "info.narazaki.android.lib.extra.ALLOW_NEW_FILE";
-
-    public static final String INTENT_KEY_CHECK_WRITABLE = "info.narazaki.android.lib.extra.CHECK_WRITABLE";
-    public static final String INTENT_KEY_WRITE_FAILED_MESSAGE = "info.narazaki.android.lib.extra.WRITE_FAILED_MESSAGE";
-
-    public static final String INTENT_KEY_NEW_FILE_CAPTION = "info.narazaki.android.lib.extra.NEW_FILE_CAPTION";
-    public static final String INTENT_KEY_NEW_FILE_TITLE = "info.narazaki.android.lib.extra.NEW_FILE_TITLE";
-
-    public static final String INTENT_KEY_NEW_DIR_CAPTION = "info.narazaki.android.lib.extra.NEW_DIR_CAPTION";
-    public static final String INTENT_KEY_NEW_DIR_TITLE = "info.narazaki.android.lib.extra.NEW_DIR_TITLE";
-
-    public static final String INTENT_KEY_FILE_PATTERN = "info.narazaki.android.lib.extra.FILE_PATTERN";
-    public static final String INTENT_KEY_FILE_EXTENTION = "info.narazaki.android.lib.extra.FILE_EXTENTION";
-
-    public static final String INTENT_KEY_ALERT_OVERWRITE = "info.narazaki.android.lib.extra.ALERT_OVERWRITE";
-    public static final String INTENT_KEY_SLECTION_ALERT_TITLE = "info.narazaki.android.lib.extra.SLECTION_ALERT_TITLE";
-    public static final String INTENT_KEY_SLECTION_ALERT_MESSAGE = "info.narazaki.android.lib.extra.SLECTION_ALERT_MESSAGE";
-
-    public static final String INTENT_KEY_RECENT_DIR_KEEP_TAG = "info.narazaki.android.lib.extra.RECENT_DIR_KEEP_TAG";
-
-    public static final String INTENT_KEY_PICK_DIRECTORY = "info.narazaki.android.lib.extra.PICK_DIRECTORY";
-    public static final String INTENT_KEY_PICK_DIR_CAPTION = "info.narazaki.android.lib.extra.PICK_DIR_CAPTION";
 
     private boolean is_light_theme_ = false;
 
