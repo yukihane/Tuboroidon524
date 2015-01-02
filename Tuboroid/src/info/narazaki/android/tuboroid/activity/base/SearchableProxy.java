462fbb27b32bdc0e022e3ce5db15c39ac55ababb
yukihane <yukihane.feather@gmail.com>
2015-01-02 14:01:23 +0900

    SearchableListActivity の機能を持つ, Activityを継承していない代理クラスを実装

--- Tuboroid/src/info/narazaki/android/tuboroid/activity/base/SearchableProxy.java
+++ Tuboroid/src/info/narazaki/android/tuboroid/activity/base/SearchableProxy.java
@@ -1,8 +1,11 @@
 package info.narazaki.android.tuboroid.activity.base;
 
 import info.narazaki.android.tuboroid.R;
+
+import java.util.Objects;
+
+import android.app.Activity;
 import android.content.Context;
-import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,9 +19,11 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 /**
- * @deprecated 代わりに {@link SearchableProxy} を使用します.
+ * {@link SearchableListActivity} の機能を代替提供することを目的としたクラスです.
+ * {@link SearchableListActivity} を使用すると無駄にActivityの継承階層が深くなり, また,
+ * Activityがfatになってしまいます.
  */
-abstract public class SearchableListActivity extends TuboroidListActivity {
+public abstract class SearchableProxy {
     public static final String TAG = "SearchableListActivity";
 
     private boolean search_bar_visible_;
@@ -28,37 +33,61 @@ abstract public class SearchableListActivity extends TuboroidListActivity {
     private int menu_id_search_bar_show_ = 0;
     private int menu_id_search_bar_hide_ = 0;
 
-    // ////////////////////////////////////////////////////////////
-    // ステート管理系
-    // ////////////////////////////////////////////////////////////
+    private final TuboroidListActivity activity;
 
-    @Override
-    protected void onCreate(final Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
+    /**
+     * @param activity
+     *            本インスタンスに処理を委譲するactivity.
+     */
+    public SearchableProxy(TuboroidListActivity activity) {
+        this.activity = Objects.requireNonNull(activity);
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
+    /**
+     * 委譲元のonResumeメソッド内で実行してください.
+     */
+    public void onResume() {
         search_bar_visible_ = true;
-        showToolBar(getTuboroidApplication().tool_bar_visible_);
+        showToolBar(activity.getTuboroidApplication().tool_bar_visible_);
         showSearchBar(false);
         createSearchButton();
     }
 
-    // ////////////////////////////////////////////////////////////////
-    // サーチバー
-    // ////////////////////////////////////////////////////////////////
-
-    @Override
+    /**
+     * 委譲元の{@link Activity#onSearchRequested()}が呼ばれた際に呼んでください.
+     */
     public boolean onSearchRequested() {
         toggleSearchBar();
+        return true;
+    }
+
+    /**
+     * 委譲元の{@link Activity#onPrepareOptionsMenu(Menu)}が呼ばれた際に呼んでください.
+     */
+    public boolean onPrepareOptionsMenu(final Menu menu) {
+
+        final MenuItem tool_bar_show = menu.findItem(menu_id_tool_bar_show_);
+        final MenuItem tool_bar_hide = menu.findItem(menu_id_tool_bar_hide_);
+
+        if (activity.getTuboroidApplication().tool_bar_visible_) {
+            tool_bar_show.setVisible(false);
+            tool_bar_hide.setVisible(true);
+        } else {
+            tool_bar_show.setVisible(true);
+            tool_bar_hide.setVisible(false);
+        }
+
+        final MenuItem search_bar_show = menu.findItem(menu_id_search_bar_show_);
+        final MenuItem search_bar_hide = menu.findItem(menu_id_search_bar_hide_);
+
+        if (search_bar_visible_) {
+            search_bar_show.setVisible(false);
+            search_bar_hide.setVisible(true);
+        } else {
+            search_bar_show.setVisible(true);
+            search_bar_hide.setVisible(false);
+        }
+
         return true;
     }
 
@@ -117,7 +146,7 @@ abstract public class SearchableListActivity extends TuboroidListActivity {
 
     private void onSubmitSearchBar() {
         final EditText edit_text = getSearchEditView();
-        final InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        final InputMethodManager imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(edit_text.getWindowToken(), 0);
         updateFilter(edit_text.getText().toString());
     }
@@ -136,19 +165,19 @@ abstract public class SearchableListActivity extends TuboroidListActivity {
     }
 
     protected LinearLayout getSearchBarView() {
-        return (LinearLayout) findViewById(R.id.search_bar);
+        return (LinearLayout) activity.findViewById(R.id.search_bar);
     }
 
     protected EditText getSearchEditView() {
-        return (EditText) findViewById(R.id.edit_search);
+        return (EditText) activity.findViewById(R.id.edit_search);
     }
 
     protected ImageButton getSearchButtonView() {
-        return (ImageButton) findViewById(R.id.button_search);
+        return (ImageButton) activity.findViewById(R.id.button_search);
     }
 
     protected ImageButton getSearchCancelButtonView() {
-        return (ImageButton) findViewById(R.id.button_cancel_search);
+        return (ImageButton) activity.findViewById(R.id.button_cancel_search);
     }
 
     abstract protected void updateFilter(final String filter);
@@ -158,17 +187,11 @@ abstract public class SearchableListActivity extends TuboroidListActivity {
     // ////////////////////////////////////////////////////////////////
 
     protected void toggleToolBar() {
-        if (getTuboroidApplication().tool_bar_visible_) {
+        if (activity.getTuboroidApplication().tool_bar_visible_) {
             showToolBar(false);
         } else {
             showToolBar(true);
         }
-    }
-
-    @Override
-    protected void createToolbarButtons() {
-        super.createToolbarButtons();
-
     }
 
     // ////////////////////////////////////////////////////////////////
@@ -183,7 +206,7 @@ abstract public class SearchableListActivity extends TuboroidListActivity {
         menu_id_search_bar_hide_ = menu_id_search_bar_hide;
 
         final MenuItem tool_bar_show = menu.add(0, menu_id_tool_bar_show_, menu_id_tool_bar_show_,
-                getString(R.string.label_menu_show_toolbar));
+                activity.getString(R.string.label_menu_show_toolbar));
         tool_bar_show.setIcon(R.drawable.ic_menu_show_toolbar);
         tool_bar_show.setOnMenuItemClickListener(new OnMenuItemClickListener() {
             @Override
@@ -194,7 +217,7 @@ abstract public class SearchableListActivity extends TuboroidListActivity {
         });
 
         final MenuItem tool_bar_hide = menu.add(0, menu_id_tool_bar_hide_, menu_id_tool_bar_hide_,
-                getString(R.string.label_menu_hide_toolbar));
+                activity.getString(R.string.label_menu_hide_toolbar));
         tool_bar_hide.setIcon(android.R.drawable.ic_menu_close_clear_cancel);
         tool_bar_hide.setOnMenuItemClickListener(new OnMenuItemClickListener() {
             @Override
@@ -205,7 +228,7 @@ abstract public class SearchableListActivity extends TuboroidListActivity {
         });
 
         final MenuItem search_bar_show = menu.add(0, menu_id_search_bar_show_, menu_id_search_bar_show_,
-                getString(R.string.label_menu_show_searchbar));
+                activity.getString(R.string.label_menu_show_searchbar));
         search_bar_show.setIcon(R.drawable.ic_menu_show_searchbar);
         search_bar_show.setOnMenuItemClickListener(new OnMenuItemClickListener() {
             @Override
@@ -216,7 +239,7 @@ abstract public class SearchableListActivity extends TuboroidListActivity {
         });
 
         final MenuItem search_bar_hide = menu.add(0, menu_id_search_bar_hide_, menu_id_search_bar_hide_,
-                getString(R.string.label_menu_hide_searchbar));
+                activity.getString(R.string.label_menu_hide_searchbar));
         search_bar_hide.setIcon(R.drawable.ic_menu_hide_searchbar);
         search_bar_hide.setOnMenuItemClickListener(new OnMenuItemClickListener() {
             @Override
@@ -227,33 +250,16 @@ abstract public class SearchableListActivity extends TuboroidListActivity {
         });
     }
 
-    @Override
-    public boolean onPrepareOptionsMenu(final Menu menu) {
-        super.onPrepareOptionsMenu(menu);
-
-        final MenuItem tool_bar_show = menu.findItem(menu_id_tool_bar_show_);
-        final MenuItem tool_bar_hide = menu.findItem(menu_id_tool_bar_hide_);
-
-        if (getTuboroidApplication().tool_bar_visible_) {
-            tool_bar_show.setVisible(false);
-            tool_bar_hide.setVisible(true);
+    protected void showToolBar(final boolean show) {
+        final LinearLayout toolbar = (LinearLayout) activity.findViewById(R.id.toolbar);
+        if (toolbar == null)
+            return;
+        if (show) {
+            toolbar.setVisibility(View.VISIBLE);
+            activity.getTuboroidApplication().tool_bar_visible_ = true;
         } else {
-            tool_bar_show.setVisible(true);
-            tool_bar_hide.setVisible(false);
+            toolbar.setVisibility(View.GONE);
+            activity.getTuboroidApplication().tool_bar_visible_ = false;
         }
-
-        final MenuItem search_bar_show = menu.findItem(menu_id_search_bar_show_);
-        final MenuItem search_bar_hide = menu.findItem(menu_id_search_bar_hide_);
-
-        if (search_bar_visible_) {
-            search_bar_show.setVisible(false);
-            search_bar_hide.setVisible(true);
-        } else {
-            search_bar_show.setVisible(true);
-            search_bar_hide.setVisible(false);
-        }
-
-        return true;
     }
-
 }
