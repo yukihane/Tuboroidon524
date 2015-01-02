37b8be0a23deafc7e49743d43861f292423dab86
yukihane <yukihane.feather@gmail.com>
2015-01-02 12:47:08 +0900

    定数定義クラス移動

--- Tuboroid/src/info/narazaki/android/tuboroid/activity/SimilarThreadListActivity.java
+++ Tuboroid/src/info/narazaki/android/tuboroid/activity/SimilarThreadListActivity.java
@@ -1,5 +1,7 @@
 package info.narazaki.android.tuboroid.activity;
 
+import static info.narazaki.android.tuboroid.contents.thread_entry_list.model.Constants.INTENT_KEY_MAYBE_ONLINE_COUNT;
+import static info.narazaki.android.tuboroid.contents.thread_entry_list.model.Constants.INTENT_KEY_MAYBE_THREAD_NAME;
 import info.narazaki.android.lib.adapter.SimpleListAdapterBase;
 import info.narazaki.android.lib.dialog.SimpleProgressDialog;
 import info.narazaki.android.lib.system.MigrationSDK5;
@@ -148,8 +150,8 @@ public class SimilarThreadListActivity extends TuboroidListActivity {
             return;
 
         intent.setData(Uri.parse(thread_data.getThreadURI()));
-        intent.putExtra(ThreadEntryListActivity.INTENT_KEY_MAYBE_THREAD_NAME, thread_data.thread_name_);
-        intent.putExtra(ThreadEntryListActivity.INTENT_KEY_MAYBE_ONLINE_COUNT, thread_data.online_count_);
+        intent.putExtra(INTENT_KEY_MAYBE_THREAD_NAME, thread_data.thread_name_);
+        intent.putExtra(INTENT_KEY_MAYBE_ONLINE_COUNT, thread_data.online_count_);
         MigrationSDK5.Intent_addFlagNoAnimation(intent);
         startActivity(intent);
     }
